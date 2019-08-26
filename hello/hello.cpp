6e96f96446a3d8d724ad2535b750221ef740e366
EmbraceLife <1227561934@qq.com>
2019-08-26 18:36:37 +0800

    mySort

---
+++ hello/hello.cpp
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
+
+
+// https://medium.com/gdplabs/build-and-debug-c-on-visual-studio-code-for-mac-77e05537105e
+// https://developer.apple.com/download/more/
